4aed562aceb3ac7ca7bf395f6170673740377630
wudg <544038230@qq.com>
2025-12-05 18:04:22 +0800

    Optimize comments

--- src/Engine/PdfImagesEngine.php
+++ src/Engine/PdfImagesEngine.php
@@ -2,21 +2,21 @@
 
 namespace Wudg\PdfImages\Engine;
 ! defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
-
+use Imagick;
 /**
  *
- * @method openImage(string $file):$this
- * @method addText(string $text, array $options = []):$this
- * @method mergeImage(string $overlayPath, array $options = []):$this
- * @method crop(int $width, int $height, int $x, int $y):$this
- * @method resize(int $width, ?int $height = null, bool $keepAspect = true):$this
- * @method blur(float $radius = 0, float $sigma = 1):$this
- * @method flip(string $mode = 'vertical'):$this
- * @method rotate(float $degrees):$this
- * @method getIm():Imagick
- * @method sharpen(float $radius = 0, float $sigma = 1):$this
- * @method combineImages(array $images, string $direction = 'v', int $spacing = 0, string $background_color = 'white'):$this
- * @method imagesToGif(array $images, int $delay = 20):$this
+ * @method $this openImage(string $file):$this
+ * @method $this addText(string $text, array $options = []):$this
+ * @method $this mergeImage(string $overlayPath, array $options = []):$this
+ * @method $this crop(int $width, int $height, int $x, int $y):$this
+ * @method $this resize(int $width, ?int $height = null, bool $keepAspect = true):$this
+ * @method $this blur(float $radius = 0, float $sigma = 1):$this
+ * @method $this flip(string $mode = 'vertical'):$this
+ * @method $this rotate(float $degrees):$this
+ * @method Imagick getIm():Imagick
+ * @method $this sharpen(float $radius = 0, float $sigma = 1):$this
+ * @method $this combineImages(array $images, string $direction = 'v', int $spacing = 0, string $background_color = 'white'):$this
+ * @method $this imagesToGif(array $images, int $delay = 20, string $transition = null, int $transitionSteps = 10):$this
  * @method toBlod()
  * @method toBase64()
  * @method toPath(string $savePath = null, string $ext = null)
